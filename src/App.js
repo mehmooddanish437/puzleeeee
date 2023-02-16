b01a869df0ec29fe46a59d5464848cd357ca624c
mehmooddanish437 <125083083+mehmooddanish437@users.noreply.github.com>
2023-02-16 10:08:03 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [isSolved, setIsSolved] = useState(false);
 
-  const [inputNumber, setinputNumber] = useState(null)
+  const [inputNumber, setinputNumber] = useState("")
 
   const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
